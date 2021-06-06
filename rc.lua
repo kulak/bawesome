4093d4200a328a6181a9c71bc73fbd709aa1c00d
Sergei Gnezdov <sergei.gnezdov@gmail.com>
2021-06-06 02:54:08 -0700

    extracted error handling

--- rc.lua
+++ rc.lua
@@ -32,36 +32,12 @@ local screenshot = require("screenshot")
 
 local battery_widget = require('awesome-battery_widget/init')
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
-
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
--- }}}
+require('error_handling')
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 -- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.init(gears.filesystem. get_configuration_dir() .. "default/theme.lua")
-
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"

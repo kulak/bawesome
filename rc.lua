75fb3dbc709e16aaf40e0a71e3cae766e0d59599
Sergei G <sergei.gnezdov@gmail.com>
2021-06-13 10:17:09 -0700

    tile is the default

--- rc.lua
+++ rc.lua
@@ -61,13 +61,13 @@ my.screenshot_delay_secs = 5
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     -- awful.layout.suit.floating,
-    awful.layout.suit.spiral,
     awful.layout.suit.tile,
+    awful.layout.suit.spiral,
     -- awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max,
     -- awful.layout.suit.max.fullscreen,
